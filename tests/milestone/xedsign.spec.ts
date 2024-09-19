872291438f4187db1dfe2bf5e883f4d3428e3495
chenxi <2465950588@qq.com>
2024-09-19 04:26:13 -0700

    test: add milestone UI e2e test

---
+++ tests/milestone/xedsign.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('milestone 组件xdesign规范', () => {
+  test('默认--UI截图', async ({ page }) => {
+    page.on('pageerror', (exception) => expect(exception).toBeNull())
+    await page.goto('milestone#basic-usage')
+    const demo = page.locator('#basic-usage .pc-demo')
+    await expect(demo).toBeInViewport()
+    await expect(demo).toHaveScreenshot('basic-usage.png')
+  })
+})
