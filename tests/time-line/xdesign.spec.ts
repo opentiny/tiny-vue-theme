b9d27582246dd86e4de9736e03db473f801c3f65
chenxi <2465950588@qq.com>
2024-09-20 01:42:31 -0700

    test: add time-line UI e2e test

---
+++ tests/time-line/xdesign.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('tabs 组件xdesign规范', () => {
+  test('节点状态 --UI截图', async ({ page }) => {
+    page.on('pageerror', (exception) => expect(exception).toBeNull())
+    await page.goto('time-line#status')
+    const demo = page.locator('#status .pc-demo')
+    await expect(demo).toBeInViewport()
+    await expect(demo).toHaveScreenshot('status.png')
+  })
+
+  test('圆点外观 --UI截图', async ({ page }) => {
+    page.on('pageerror', (exception) => expect(exception).toBeNull())
+    await page.goto('time-line#shape')
+    const demo = page.locator('#shape .pc-demo')
+    await expect(demo).toBeInViewport()
+    await expect(demo).toHaveScreenshot('shape.png')
+  })
+})
