538882c33f0eea493f31db444e9f0c9dd7ae30f7
ajaxzheng <894103554@qq.com>
2024-09-28 14:58:56 +0800

    test(e2e): add collapse UI e2e test

--- tests/collapse/xdesign.spec.ts
+++ tests/collapse/xdesign.spec.ts
@@ -8,11 +8,20 @@ test.describe('collapse折叠面板xdesign规范', () => {
     await expect(demo).toBeInViewport()
     await expect(demo).toHaveScreenshot('basic-usage.png')
   })
+
   test('禁用--UI截图', async ({ page }) => {
     page.on('pageerror', (exception) => expect(exception).toBeNull())
     await page.goto('collapse#disable')
     const demo = page.locator('#disable .pc-demo')
     await expect(demo).toBeInViewport()
     await expect(demo).toHaveScreenshot('disabled.png')
+  })
+
+  test('面板标题--UI截图', async ({ page }) => {
+    page.on('pageerror', (exception) => expect(exception).toBeNull())
+    await page.goto('collapse#title')
+    const demo = page.locator('#title .pc-demo')
+    await expect(demo).toBeInViewport()
+    await expect(demo).toHaveScreenshot('title.png')
   })
 })
